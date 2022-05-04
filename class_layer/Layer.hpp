ff230d421aaef8989d0cf2a927f7e8a3584a60e7
Asahi <78383580+asahi-kojima@users.noreply.github.com>
2022-05-04 21:31:37 +0900

    Add files via upload

---
+++ class_layer/Layer.hpp
@@ -0,0 +1,11 @@
+#ifndef ___LAYER
+#define ___LAYER
+
+#include "Base_Layer.hpp"
+#include "Relu.hpp"
+#include "Affine.hpp"
+#include "SoftmaxWithLoss.hpp"
+#include "BatchNormal.hpp"
+#include "Convolution.hpp"
+
+#endif
