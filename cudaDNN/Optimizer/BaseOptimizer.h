374a0143ffe357f603be13e52dc1c1f40b5492fc
AsahiKojima <ubutara12@gmail.com>
2023-03-14 22:58:57 +0900

    add BaseOptimizer

---
+++ cudaDNN/Optimizer/BaseOptimizer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+
+#define SINGLETON(CLASSNAME) \
+private:\
+	CLASSNAME() = default;\
+	~CLASSNAME() = default;\
+public:\
+	CLASSNAME(const CLASSNAME&) = delete;\
+	CLASSNAME& operator=(const CLASSNAME&) = delete;\
+	CLASSNAME(CLASSNAME&&) = delete;\
+	CLASSNAME& operator=(CLASSNAME&&) = delete;\
+	static CLASSNAME& getInstance()\
+	{\
+		static CLASSNAME instance;\
+		return instance;\
+	}
+
+namespace miduho
+{
+	namespace layer
+	{
+		class BaseLayer;
+	}
+	namespace optimizer
+	{
+		class BaseOptimizer
+		{
+		public:
+			BaseOptimizer() = default;
+			~BaseOptimizer() = default;
+			virtual void optimize(layer::BaseLayer*) = 0;
+		};
+	}
+}
